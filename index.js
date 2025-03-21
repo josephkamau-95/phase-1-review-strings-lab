f26e2c515df84b9bd32686cfeda7bcd01bafe19e
Joseph Mwangi <josymuramu@gmail.com>
2025-03-21 14:47:42 +0300

    completed the strings lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Muramu Mayai"
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+const upperCaseCurrentUser = currentUser.toUpperCase()
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!"
+let shortGreeting = "Welcome, M!"
